262fe2249316f03f33734e2d6f38a750efaa8c5e
synaxxkhushi <149578930+synaxxkhushi@users.noreply.github.com>
2025-06-26 01:50:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", "24267726"))
-API_HASH = environ.get("API_HASH", "7500ba8248548cc3864bd033668f9a9a")
+API_ID = int(environ.get("API_ID", "14050586"))
+API_HASH = environ.get("API_HASH", "42a60d9c657b106370c79bb0a8ac560c")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 # Make Bot Admin In Log Channel With Full Rights
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002053069822"))
-ADMINS = int(environ.get("ADMINS", "6231550362"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002627664136"))
+ADMINS = int(environ.get("ADMINS", "7998441787"))
 
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_URI = environ.get("DB_URI", "mongodb+srv://EXONTESTMONGO:EXONTESTMONGO@cluster0.bviw7ic.mongodb.net/?retryWrites=true&w=majority") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
